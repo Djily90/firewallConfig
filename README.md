96a26693c1a17dcbcd5462472354a591a07bbd93
Djily SARR <djilysarr275@gmail.com>
2024-02-22 14:53:45 +0000

    plugin v1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Firewall configuration
-Retrieve firewall configurations
+Retrieve the firewall configuration
---
+++ install.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_firewallconfig()
+{
+    $commonObject = new ExtensionCommon;
+
+    // Remove older tables
+    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `firewallconfig`;");
+
+    // Install tables
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `firewallconfig` (
+        `ID` int(11) NOT NULL AUTO_INCREMENT,
+        `HARDWARE_ID` int(11) NOT NULL,
+        `PROFILE` varchar(255) DEFAULT NULL,
+        `STATE` varchar(255) DEFAULT NULL,
+        `FIREWALLPOLICY` varchar(255) DEFAULT NULL,
+        `LOCALFIREWALLRULES` int(11) DEFAULT NULL,
+        `FILENAME` varchar(255) DEFAULT NULL,
+        `MAXFILESIZE` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`ID`,`HARDWARE_ID`)
+      )  ENGINE=INNODB ;");
+      
+}
+
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_firewallconfig()
+{
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `firewallconfig`;");
+}
+
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_firewallconfig()
+{
+
+}
